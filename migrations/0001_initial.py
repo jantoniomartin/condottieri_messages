d2d7f9cbc6b72fed76bd1b42c16a77863c23f2d4
José Antonio Martín Prieto <jantonio.martin@gmail.com>
2018-02-02 22:39:12 +0100

    added django migrations

---
+++ migrations/0001_initial.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('machiavelli', '__first__'),
+        ('django_messages', '0002_auto_20160607_0852'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Letter',
+            fields=[
+                ('message_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='django_messages.Message')),
+                ('year', models.PositiveIntegerField(default=0)),
+                ('season', models.PositiveIntegerField(default=1, choices=[(1, 'Spring'), (2, 'Summer'), (3, 'Fall')])),
+                ('recipient_player', models.ForeignKey(related_name='received_messages', verbose_name='To country', to='machiavelli.Player')),
+                ('sender_player', models.ForeignKey(related_name='sent_messages', verbose_name='From country', to='machiavelli.Player')),
+            ],
+            bases=('django_messages.message',),
+        ),
+    ]
--- models.py
+++ models.py
@@ -25,48 +25,46 @@ from machiavelli.models import Player, Revolution, SEASONS
 from machiavelli.signals import government_overthrown
 
 if "notification" in settings.INSTALLED_APPS:
-	from notification import models as notification
+        from notification import models as notification
 else:
-	notification = None
+        notification = None
 
 class Letter(messages.Message):
-	""" Letter is just a django-messages Message that adds two fields: sender_player and
-	recipient_player. """
-	sender_player = models.ForeignKey(Player, related_name='sent_messages', verbose_name=_("From country"))
-	recipient_player = models.ForeignKey(Player, related_name='received_messages', verbose_name=_("To country"))
-	year = models.PositiveIntegerField(default=0)
-	season = models.PositiveIntegerField(default=1, choices=SEASONS)
+        """ Letter is just a django-messages Message that adds two fields: sender_player and
+        recipient_player. """
+        sender_player = models.ForeignKey(Player, related_name='sent_messages', verbose_name=_("From country"))
+        recipient_player = models.ForeignKey(Player, related_name='received_messages', verbose_name=_("To country"))
+        year = models.PositiveIntegerField(default=0)
+        season = models.PositiveIntegerField(default=1, choices=SEASONS)
     
-	def save(self, *args, **kwargs):
-		if not self.id:
-			self.sender = self.sender_player.user
-			self.recipient = self.recipient_player.user
-			self.year = self.sender_player.game.year
-			self.season = self.sender_player.game.season
-		super(Letter, self).save(*args, **kwargs)
-	
-	def get_absolute_url(self):
-		return ('condottieri_messages_detail', [self.id])
-	get_absolute_url = models.permalink(get_absolute_url)
+        def save(self, *args, **kwargs):
+                if not self.id:
+                        self.sender = self.sender_player.user
+                        self.recipient = self.recipient_player.user
+                        self.year = self.sender_player.game.year
+                        self.season = self.sender_player.game.season
+                super(Letter, self).save(*args, **kwargs)
+        
+        def get_absolute_url(self):
+                return ('condottieri_messages_detail', [self.id])
+        get_absolute_url = models.permalink(get_absolute_url)
 
-def notify_new_letter(sender, instance, created, **kw):
-	if notification and isinstance(instance, Letter) and created:
-		user = [instance.recipient,]
-		game = instance.recipient_player.game
-		extra_context = {'game': game,
-						'letter': instance,
-						'STATIC_URL': settings.STATIC_URL,}
-		if game.fast:
-			notification.send_now(user, "condottieri_messages_received", extra_context)
-		else:
-			notification.send(user, "condottieri_messages_received", extra_context)
+def notify_new_letter(sender, instance, created, raw, **kw):
+    if notification and isinstance(instance, Letter) and created and not raw:
+        user = [instance.recipient,]
+        game = instance.recipient_player.game
+        extra_context = {'game': game, 'letter': instance, 'STATIC_URL': settings.STATIC_URL,}
+        if game.fast:
+            notification.send_now(user, "condottieri_messages_received", extra_context)
+        else:
+            notification.send(user, "condottieri_messages_received", extra_context)
 
 models.signals.post_save.connect(notify_new_letter, sender=Letter)
 
 def update_letter_users(sender, **kwargs):
-	assert isinstance(sender, Revolution), "sender must be a Revolution"
-	Letter.objects.filter(sender_player=sender.government_player).update(sender=sender.opposition)
-	Letter.objects.filter(recipient_player=sender.government_player).update(recipient=sender.opposition)
+        assert isinstance(sender, Revolution), "sender must be a Revolution"
+        Letter.objects.filter(sender_player=sender.government_player).update(sender=sender.opposition)
+        Letter.objects.filter(recipient_player=sender.government_player).update(recipient=sender.opposition)
 
 government_overthrown.connect(update_letter_users)
 
