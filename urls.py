4f3fb52dfdfb7f7858d468b6ab5cf8d106f05ad9
José Antonio Martín Prieto <jantonio.martin@gmail.com>
2019-09-08 23:07:32 +0200

    removed django.conf.url.patterns

--- urls.py
+++ urls.py
@@ -1,24 +1,23 @@
-from django.conf.urls import *
+from django.conf.urls import url
 from django.views.generic.base import RedirectView
 
-import condottieri_messages.views as views
-from condottieri_messages.views import BoxListView
+from . import views
 
 ## urls that call the views in messages
-urlpatterns = patterns('messages.views',
+urlpatterns = [
     url(r'^$', RedirectView.as_view(url='inbox/')),
-)
+]
 
 ## urls that call the custom views in condottieri_messages
-urlpatterns += patterns('condottieri_messages.views',
-	url(r'^compose/(?P<sender_id>[\d]+)/(?P<recipient_id>[\d]+)/$', 'compose', name='condottieri_messages_compose'),
-    url(r'^reply/(?P<letter_id>[\d]+)/$', 'compose', name='condottieri_messages_reply'),
-    url(r'^view/(?P<message_id>[\d]+)/$', 'view', name='condottieri_messages_detail'),
-	url(r'^inbox/$', BoxListView.as_view(box='inbox'), name='messages_inbox'),
-	url(r'^outbox/$', BoxListView.as_view(box='outbox'), name='messages_outbox'),
-	url(r'^trash/$', BoxListView.as_view(box='trash'), name='messages_trash'),
-	url(r'^inbox/(?P<slug>[-\w]+)/$', BoxListView.as_view(box='inbox') , name='condottieri_messages_inbox'),
-	url(r'^outbox/(?P<slug>[-\w]+)/$', BoxListView.as_view(box='outbox') , name='condottieri_messages_outbox'),
-    url(r'^delete/(?P<message_id>[\d]+)/$', 'delete', name='messages_delete'),
-    url(r'^undelete/(?P<message_id>[\d]+)/$', 'undelete', name='messages_undelete'),
-)
+urlpatterns += [
+	url(r'^compose/(?P<sender_id>[\d]+)/(?P<recipient_id>[\d]+)/$', views.compose, name='condottieri_messages_compose'),
+    url(r'^reply/(?P<letter_id>[\d]+)/$', views.compose, name='condottieri_messages_reply'),
+    url(r'^view/(?P<message_id>[\d]+)/$', views.view , name='condottieri_messages_detail'),
+	url(r'^inbox/$', views.BoxListView.as_view(box='inbox'), name='messages_inbox'),
+	url(r'^outbox/$', views.BoxListView.as_view(box='outbox'), name='messages_outbox'),
+	url(r'^trash/$', views.BoxListView.as_view(box='trash'), name='messages_trash'),
+	url(r'^inbox/(?P<slug>[-\w]+)/$', views.BoxListView.as_view(box='inbox') , name='condottieri_messages_inbox'),
+	url(r'^outbox/(?P<slug>[-\w]+)/$', views.BoxListView.as_view(box='outbox') , name='condottieri_messages_outbox'),
+    url(r'^delete/(?P<message_id>[\d]+)/$', views.delete, name='messages_delete'),
+    url(r'^undelete/(?P<message_id>[\d]+)/$', views.undelete, name='messages_undelete'),
+]
